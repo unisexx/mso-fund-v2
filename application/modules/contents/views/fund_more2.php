7b0889cebeea3408758af088e075ac7f9f810c33
Dear <unisexx@gmail.com>
2017-09-26 11:48:22 +0700

    แก้ไข หน้าปราบปรามค้ามนุษย์

--- application/modules/contents/views/fund_more2.php
+++ application/modules/contents/views/fund_more2.php
@@ -1,69 +1,102 @@
-<div id="title-blank"><?=$_GET['module']?></div>
+<div id="title-blank">
+    <?=$_GET['module']?>
+</div>
 <div id="breadcrumb"><a href="#">หน้าแรก</a> > <span class="b1"><?=$_GET['module']?></span></div>
 <div id="page">
- <br>
-  <div id="page-colleft">
-	<div id="title-page-colleft">เกี่ยวกับกองทุน</div>
-      <ul>
-      
-       	  <li>
-          
-          <img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" />
-          
-           <a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=เกี่ยวกับกองทุน">
+    <br>
+    <div id="page-colleft">
+        <div id="title-page-colleft">เกี่ยวกับกองทุน</div>
+        <ul>
+
+            <li>
+
+                <img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" />
+
+                <a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=เกี่ยวกับกองทุน">
            
            เกี่ยวกับกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์
            
            </a>
-           
-          </li>
-          
-          <li>
-          
-          <img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" />
-          
-           <a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=โครงสร้างการบริหารกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์">
+
+            </li>
+
+            <li>
+
+                <img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" />
+
+                <a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=โครงสร้างการบริหารกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์">
            
            โครงสร้างการบริหารกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์
            
            </a>
-           
-          </li>
-          
-           <li>
-          
-          <img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" />
-          
-           <a href="uploads/pdf/mso_data_00.pdf" target="_blank">
+
+            </li>
+
+            <!-- <li>
+
+                <img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" />
+
+                <a href="uploads/pdf/mso_data_00.pdf" target="_blank">
            
            แผ่นพับประชาสัมพันธ์กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์
            
            </a>
-           
-          </li>
-          
-          
-          
-      </ul>
-<br><br>
-          <div id="title-page-colleft-2">ข่าวกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์</div>
-          <div id="colleft-2">
-          <ul>
-              <!-- <li><a href="#">กลยุทธ์กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย</a></li>
-              <li><a href="#">กลยุทธ์กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย</a></li> -->
-          </ul>
-  </div>
+
+            </li> -->
+
+
+
+        </ul>
+        <br><br>
+        <div id="title-page-colleft">ข่าวกองประชาสัมพันธ์กองทุน</div>
+        <div id="colleft">
+            <ul>
+                <li><a href="uploads/pdf/แนวทางการขอรับการสนับสนุน.pdf" target="_blank"><img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" /> แนวทางการขอรับการสนับสนุน</a></li>
+                <li><a href="uploads/pdf/แผ่นพับกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุาย์.pdf" target="_blank"><img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" /> แผ่นพับกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์</a></li>
+                <li><a href="uploads/pdf/วาสารกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์_รอบ_6_เดือน.pdf" target="_blank"><img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" /> วาสารกองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์ รอบ 6 เดือน</a></li>
+            </ul>
+        </div>
+
+        <br><br>
+        <div id="title-page-colleft">ข่าวประชาสัมพันธ์</div>
+        <div id="colleft">
+            <ul>
+                <li><a href="https://www.m-society.go.th/ewt_news.php?nid=19496"><img src="themes/fundv2/images/banner-fund/icon-megaphone.png" width="22" height="22" /> นโยบายการกำกับดูแลองค์การที่ดี</a></li>
+            </ul>
+        </div>
     </div>
-    	 
+
     <div id="page-colright">
-    	<div id="budjet1_banner1"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=การขอรับเงินสนับสนุน"><img src="themes/fundv2/images/banner-fund/budjet2_banner1.png" width="249" height="143" /></a></div>
+        <div id="budjet1_banner1"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=การขอรับเงินสนับสนุน"><img src="themes/fundv2/images/banner-fund/budjet2_banner1.png" width="249" height="143" /></a></div>
         <div id="budjet1_banner2"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=เกณฑ์การพิจารณา"><img src="themes/fundv2/images/banner-fund/budjet2_banner2.png" width="248" height="153" /></a></div>
         <div id="budjet1_banner3"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=แบบฟอร์มที่เกี่ยวกับกองทุน (ดาวน์โหลดแบบฟอร์ม)"><img src="themes/fundv2/images/banner-fund/budjet2_banner3.png" width="251" height="144" /></a></div>
         <div id="budjet1_banner4"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=ระเบียบและประกาศที่เกี่ยวข้อง"><img src="themes/fundv2/images/banner-fund/budjet2_banner4.png" width="256" height="151" /></a></div>
         <div id="budjet1_banner5"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=การเงินและการบริหารงบประมาณ"><img src="themes/fundv2/images/banner-fund/budjet2_banner5.png" width="248" height="153" /></a></div>
         <div id="budjet1_banner6"><a target="_blank" href="contents/view?module=กองทุนเพื่อการป้องกันและปราบปรามการค้ามนุษย์&category=ผลการดำเนินงานกองทุน"><img src="themes/fundv2/images/banner-fund/budjet2_banner6.png" width="248" height="153" /></a></div>
     </div>
-		  <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-          <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
 </div>
